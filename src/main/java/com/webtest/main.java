9eee072eb947dbfa2ac5e1f9ebf0ac5240f6e1da
JElorza <jelorza@impronta.es>
2019-06-10 18:42:15 +0200

    test actualizados

---
+++ src/main/java/com/webtest/main.java
@@ -0,0 +1,9 @@
+package com.webtest;
+
+public class main {
+	
+	public main() {
+		System.out.println("Main");
+	}
+
+}
---
+++ src/test/java/test/webtest/primerTest.java
@@ -0,0 +1,21 @@
+package test.webtest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class primerTest {
+	
+	@Test
+	public void firstTest() {
+		
+		assertEquals("1","1");
+	}
+	
+	@Test
+	public void secondTest() {
+		
+		assertEquals("1","1");
+	}
+
+}
